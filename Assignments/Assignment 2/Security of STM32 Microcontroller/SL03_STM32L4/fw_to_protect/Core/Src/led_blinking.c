15e0c45439169acbea39821c16a5d759ceed4f8e
Angelo Nolasco <angelon3121@gmail.com>
2023-03-08 14:26:12 -0800

    Signed-off-by: Angelo Nolasco <angelon3121@gmail.com> AS2-Lab(1-3)

---
+++ Assignments/Assignment 2/Security of STM32 Microcontroller/SL03_STM32L4/fw_to_protect/Core/Src/led_blinking.c
@@ -0,0 +1,15 @@
+/*
+ * led_blinking.c
+ *
+ * Created on: Feb 28,2023
+ * 		Author : Angelo
+ */
+
+#include "main.h"
+
+uint32_t cpt;
+
+void secret_led_blinking(void){
+	cpt++;
+	HAL_GPIO_TogglePin(LD2_GPIO_Port,LD2_Pin);
+}
